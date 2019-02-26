d098fe497a516434cf7bb1db7f9bf0b2e976666d
Maximilian <10422779+MaximilianGrw@users.noreply.github.com>
2019-02-26 16:37:13 +0100

    Create WIN10_uninstall_microsoft-apps.sh

---
+++ windows/WIN10_uninstall_microsoft-apps.sh
@@ -0,0 +1,35 @@
+// Kamera
+Get-AppxPackage *camera* | Remove-AppxPackage
+
+3D Builder
+Get-AppxPackage *3d* | Remove-AppxPackage
+
+Kamera
+Get-AppxPackage *camera* | Remove-AppxPackage
+
+Mail und Kalender
+Get-AppxPackage *communi* | Remove-AppxPackage
+
+Nachrichten, Sport, Finanzen und Wetter
+Get-AppxPackage *bing* | Remove-AppxPackage
+
+Groove-Musik, Filme & Fernsehsendungen
+Get-AppxPackage *zune* | Remove-AppxPackage
+
+Kontakte
+Get-AppxPackage *people* | Remove-AppxPackage
+
+Phone Companion
+Get-AppxPackage *phone* | Remove-AppxPackage
+
+Fotos
+Get-AppxPackage *photo* | Remove-AppxPackage
+
+Microsoft Solitaire Collection
+Get-AppxPackage *solit* | Remove-AppxPackage
+
+Sprachrekorder
+Get-AppxPackage *soundrec* | Remove-AppxPackage
+
+Xbox
+Get-AppxPackage *xbox* | Remove-AppxPackage

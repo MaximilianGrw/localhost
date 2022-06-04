cacc4e938dbdf1fea530b132a757f90a3d180ae9
Maximilian <10422779+MaximilianGrw@users.noreply.github.com>
2022-06-04 15:41:25 +0200

    Update winget.ps1

--- windows/winget.ps1
+++ windows/winget.ps1
@@ -35,3 +35,4 @@ winget install TeamSpeakSystems.TeamSpeakClient   winget upgrade TeamSpeakSystem
 winget install Discord.Discord                    winget upgrade Discord.Discord
 winget install ApacheFriends.Xampp                winget upgrade ApacheFriends.Xampp
 winget install WinSCP.WinSCP                      winget upgrade WinSCP.WinSCP
+Test

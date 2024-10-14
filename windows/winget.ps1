a2014da722e33ecf1d3d0b402777a4456eee7bc4
Maximilian <10422779+MaximilianGrw@users.noreply.github.com>
2024-10-14 15:53:19 +0200

    Update winget.ps1

--- windows/winget.ps1
+++ windows/winget.ps1
@@ -5,7 +5,8 @@ winget install PuTTY.PuTTY                        winget upgrade PuTTY.PuTTY
 winget install Microsoft.PowerToys                winget upgrade Microsoft.PowerToys
 winget install vscode                             winget upgrade vscode
 winget install sumatrapdf                         winget upgrade sumatrapdf
-:: winget install TimKosse.FileZilla.Client       ::winget upgrade TimKosse.FileZilla.Client  :: currently not in the package management
+winget install Notepad++.Notepad++                winget upgrade Notepad++.Notepad++
+winget install Greenshot.Greenshot                winget upgrade Greenshot.Greenshot
 winget install IrfanSkiljan.IrfanView             winget upgrade IrfanSkiljan.IrfanView
 winget install OBSProject.OBSStudio               winget upgrade OBSProject.OBSStudio
 winget install Spotify.Spotify                    winget upgrade Spotify.Spotify
@@ -27,25 +28,25 @@ winget install Mozilla.Firefox.ESR.de             winget upgrade Mozilla.Firefox
 winget install Mozilla.Firefox                    winget upgrade Mozilla.Firefox
 winegt install Microsoft.Edge                     winget upgrade Microsoft.Edge
 
-winget install Notepad++.Notepad++                winget upgrade Notepad++.Notepad++
-winget install Greenshot.Greenshot                winget upgrade Greenshot.Greenshot
+
 winget install Garmin.GarminExpress               winget upgrade Garmin.GarminExpress
 
-winget install REALiX.HWiNFO                      winget upgrade REALiX.HWiNFO
 winget install Microsoft.Teams                    winget upgrade Microsoft.Teams
 winget install Microsoft.WindowsTerminal          winget upgrade Microsoft.WindowsTerminal
-
-
 
 winget install TeamViewer.TeamViewer              winget upgrade TeamViewer.TeamViewer
 winget install Apple.iTunes                       winget upgrade Apple.iTunes
 
+:: monitoring
+winget install REALiX.HWiNFO                      winget upgrade REALiX.HWiNFO
 winget install TechPowerUp.GPU-Z                  winget upgrade TechPowerUp.GPU-Z
 winget install CPUID.CPU-Z                        winget upgrade CPUID.CPU-Z
 
 winget install TeamSpeakSystems.TeamSpeakClient   winget upgrade TeamSpeakSystems.TeamSpeakClient 
 winget install Discord.Discord                    winget upgrade Discord.Discord
 winget install ApacheFriends.Xampp                winget upgrade ApacheFriends.Xampp
+
+:: winget install TimKosse.FileZilla.Client       ::winget upgrade TimKosse.FileZilla.Client  :: currently not in the package management
 winget install WinSCP.WinSCP                      winget upgrade WinSCP.WinSCP
 winget install WireGuard.WireGuard                winget upgrade WireGuard.WireGuard
 

e7f2b14f483a18e58ffa1d11a13726fc25e36874
Maximilian <10422779+MaximilianGrw@users.noreply.github.com>
2024-10-16 09:10:59 +0200

    Update winget.ps1

--- windows/winget.ps1
+++ windows/winget.ps1
@@ -1,26 +1,7 @@
 :: default
 winget install videoLAN.vlc                       winget upgrade videoLAN.vlc
 winget install 7zip.7zip                          winget upgrade 7zip.7zip
-winget install PuTTY.PuTTY                        winget upgrade PuTTY.PuTTY
-winget install Microsoft.PowerToys                winget upgrade Microsoft.PowerToys
-winget install vscode                             winget upgrade vscode
-winget install sumatrapdf                         winget upgrade sumatrapdf
-winget install Notepad++.Notepad++                winget upgrade Notepad++.Notepad++
 winget install Greenshot.Greenshot                winget upgrade Greenshot.Greenshot
-winget install IrfanSkiljan.IrfanView             winget upgrade IrfanSkiljan.IrfanView
-winget install OBSProject.OBSStudio               winget upgrade OBSProject.OBSStudio
-winget install Spotify.Spotify                    winget upgrade Spotify.Spotify
-winget install Valve.Steam                        winget upgrade Valve.Steam
-winget install CodeSector.TeraCopy                winget upgrade CodeSector.TeraCopy
-winget install GitHub.GitHubDesktop               winget upgrade GitHub.GitHubDesktop
-winget install Oracle.JavaRuntimeEnvironment      winget upgrade Oracle.JavaRuntimeEnvironment
-winget install WinMerge.WinMerge                  winget upgrade WinMerge.WinMerge
-
-:: network diagnostic
-winget install WiresharkFoundation.Wireshark      winget upgrade WiresharkFoundation.Wireshark
-winget install Insecure.Npcap                     winget upgrade Insecure.Npcap
-winget install Famatech.AdvancedIPScanner         winget upgrade Famatech.AdvancedIPScanner
-winget install StefanFreischlad.Filius            winget upgrade StefanFreischlad.Filius
 
 :: browser
 winget install Google.Chrome                      winget upgrade Google.Chrome
@@ -28,13 +9,45 @@ winget install Mozilla.Firefox.ESR.de             winget upgrade Mozilla.Firefox
 winget install Mozilla.Firefox                    winget upgrade Mozilla.Firefox
 winegt install Microsoft.Edge                     winget upgrade Microsoft.Edge
 
+winget install Microsoft.PowerToys                winget upgrade Microsoft.PowerToys
+
+winget install IrfanSkiljan.IrfanView             winget upgrade IrfanSkiljan.IrfanView
+winget install OBSProject.OBSStudio               winget upgrade OBSProject.OBSStudio
+winget install Spotify.Spotify                    winget upgrade Spotify.Spotify
+
+
+:: file management
+winget install CodeSector.TeraCopy                winget upgrade CodeSector.TeraCopy
+winget install WinMerge.WinMerge                  winget upgrade WinMerge.WinMerge
+
+:: office
+winget install sumatrapdf                         winget upgrade sumatrapdf
+winget install Microsoft.Teams                    winget upgrade Microsoft.Teams
+
+:: development
+winget install vscode                             winget upgrade vscode
+winget install Notepad++.Notepad++                winget upgrade Notepad++.Notepad++
+winget install ApacheFriends.Xampp                winget upgrade ApacheFriends.Xampp
+winget install GitHub.GitHubDesktop               winget upgrade GitHub.GitHubDesktop
+
+winget install Oracle.JavaRuntimeEnvironment      winget upgrade Oracle.JavaRuntimeEnvironment
+
+:: network
+
+winget install WiresharkFoundation.Wireshark      winget upgrade WiresharkFoundation.Wireshark
+winget install Insecure.Npcap                     winget upgrade Insecure.Npcap
+winget install Famatech.AdvancedIPScanner         winget upgrade Famatech.AdvancedIPScanner
+winget install StefanFreischlad.Filius            winget upgrade StefanFreischlad.Filius
+
+:: remote access
+winget install PuTTY.PuTTY                        winget upgrade PuTTY.PuTTY
+winget install TeamViewer.TeamViewer              winget upgrade TeamViewer.TeamViewer
+winget install WinSCP.WinSCP                      winget upgrade WinSCP.WinSCP
+:: winget install TimKosse.FileZilla.Client       ::winget upgrade TimKosse.FileZilla.Client  :: currently not in the package management
 
 winget install Garmin.GarminExpress               winget upgrade Garmin.GarminExpress
-
 winget install Microsoft.Teams                    winget upgrade Microsoft.Teams
 winget install Microsoft.WindowsTerminal          winget upgrade Microsoft.WindowsTerminal
-
-winget install TeamViewer.TeamViewer              winget upgrade TeamViewer.TeamViewer
 winget install Apple.iTunes                       winget upgrade Apple.iTunes
 
 :: monitoring
@@ -42,12 +55,12 @@ winget install REALiX.HWiNFO                      winget upgrade REALiX.HWiNFO
 winget install TechPowerUp.GPU-Z                  winget upgrade TechPowerUp.GPU-Z
 winget install CPUID.CPU-Z                        winget upgrade CPUID.CPU-Z
 
+:: gaming
 winget install TeamSpeakSystems.TeamSpeakClient   winget upgrade TeamSpeakSystems.TeamSpeakClient 
 winget install Discord.Discord                    winget upgrade Discord.Discord
-winget install ApacheFriends.Xampp                winget upgrade ApacheFriends.Xampp
+winget install Valve.Steam                        winget upgrade Valve.Steam
 
-:: winget install TimKosse.FileZilla.Client       ::winget upgrade TimKosse.FileZilla.Client  :: currently not in the package management
-winget install WinSCP.WinSCP                      winget upgrade WinSCP.WinSCP
+:: vpn
 winget install WireGuard.WireGuard                winget upgrade WireGuard.WireGuard
 
 pause
